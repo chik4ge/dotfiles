c318eac4176dbfe76a7fdd741753e173bd919340
chikage <35529908+chik4ge@users.noreply.github.com>
2024-11-12 00:11:10 +0900

    Update .config/nvim/lua/vim-options.lua

--- dot_config/nvim/lua/vim-options.lua
+++ dot_config/nvim/lua/vim-options.lua
@@ -3,6 +3,12 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set nu rnu")
+vim.cmd("set cursorline")
+
+vim.cmd("set whichwrap+=<,>,[,],h,l")
+vim.cmd("set scrolloff=8")
+
+vim.cmd("set clipboard=unnamed,unnamedplus")
 
 vim.g.mapleader = " "
 
